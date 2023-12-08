6831a2e7d29c0fc775013117a934da9d99445035
4Mariza <maria.luizagomedds1@gmail.com>
2023-12-08 20:10:01 -0300

    em andamento...

--- Tela Criar Conta/create.js
+++ Tela Criar Conta/create.js
@@ -1,38 +1,69 @@
-'use strict'
+"use strict";
+
+const imgPerfil = document.getElementById("img-perfil");
+const button = document.getElementById("botao-criar");
+
+let foto
 
 const handlePhone = (event) => {
-    let input = event.target
-    input.value = phoneMask(input.value)
-  }
-  
-  const phoneMask = (value) => {
-    if (!value) return ""
-    value = value.replace(/\D/g,'')
-    value = value.replace(/(\d{2})(\d)/,"($1) $2")
-    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
-    return value
-  }
+  let input = event.target;
+  input.value = phoneMask(input.value);
+};
 
-  async function cadastrarUsuario(usuario){
-    const url = 'http://localhost:8080/usuarios'
-    const options = {
-        method:'POST',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body: JSON.stringify(usuario)
-    }
-    const response = await fetch (url,options)
-    console.log(response.ok)
-    return response.ok
+const phoneMask = (value) => {
+  if (!value) return "";
+  value = value.replace(/\D/g, "");
+  value = value.replace(/(\d{2})(\d)/, "($1) $2");
+  value = value.replace(/(\d)(\d{4})$/, "$1-$2");
+  return value;
+};
+
+async function cadastrarUsuario(usuario) {
+  const url = "http://localhost:8080/usuarios";
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(usuario),
+  };
+  const response = await fetch(url, options);
+  console.log(response.ok);
+  return response.ok;
 }
 
-// TESTE
-const usuarioNovo = {
-    nome:'jose',
-    email:'jose@gmail.com',
-    telefone:'(11)94577-1205',
-    foto:'../img/',
-    senha:'abcdef74'
-}
-cadastrarUsuario(usuarioNovo)
+imgPerfil.addEventListener("change", () => {
+  let file = imgPerfil.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.addEventListener("load", (e) => {
+      const render = e.target;
+      const img = document.getElementById("img-user");
+      foto = render.result
+      img.src = foto;
+    });
+
+    reader.readAsDataURL(file);
+  } 
+});
+
+button.addEventListener("click", () => {
+  const senha = document.getElementById("senha").value;
+
+  if (senha == document.getElementById("confirmacao").value) {
+    const user = {
+      nome: document.getElementById("nome").value,
+      email: document.getElementById("email").value,
+      telefone: document.getElementById("telefone").value,
+      foto,
+      senha,
+    };
+
+    cadastrarUsuario(user);
+  }else {
+    alert('Senhas devem ser iguais!!')
+  }
+});
+
---
+++ Tela de Acesso/acesso.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const button = document.getElementById('botao-entrar')
+
+button.addEventListener('click', () =>{
+    
+})
