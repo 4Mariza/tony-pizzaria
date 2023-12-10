488522616d0e00f7b1430fdffaf03c374908842b
4Mariza <maria.luizagomedds1@gmail.com>
2023-12-10 13:01:31 -0300

    em andamento...

--- Tela de Acesso/acesso.js
+++ Tela de Acesso/acesso.js
@@ -1,7 +1,22 @@
 'use strict'
 
-const button = document.getElementById('botao-entrar')
+const login = document.getElementById('login')
+const email = document.getElementById('email')
+const senha = document.getElementById('password')
 
-button.addEventListener('click', () =>{
-    
+login.addEventListener('submit', async (e) =>{
+    e.preventDefault()
+    const url = 'http://localhost:8080/login'
+    const response = await fetch(url, {
+        body: JSON.stringify({email: email.value, senha: senha.value}),
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json'
+        }
+    })
+    const data = await response.json()
+
+    console.log(data);
+    if(response.status == 200)
+        window.location.href = "../Tela Home/home.html"
 })
